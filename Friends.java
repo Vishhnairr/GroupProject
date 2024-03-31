f97bcab716a9c6b094317c5fc395d32af2ec4c63
gil10Coding <115641776+gil10Coding@users.noreply.github.com>
2024-03-31 18:32:45 -0400

    Update Friends.java

--- Friends.java
+++ Friends.java
@@ -4,13 +4,18 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * Project 4 - Friends
+ * Friends
  *
  * This class extends the User class and allows for a user to make, accept, and decline
  * friend requests, block other users, view their friends list, and remove friends from
  * the list. The friends list is stored as a file associated with the user and is updated
  * every time they perform one of the above actions.
  *
+ *
+ * @author Lisa Luo, Zixian Liu, Viswanath Nair, Braeden Patterson, Alexia Gil, lab sec 13
+ *
+ * @version March 31, 2024
+ * 
  */
 
 public class Friends extends User {

03b4c8608f81058e9b80a07f38cd4e11dd7297c0
gil10Coding <115641776+gil10Coding@users.noreply.github.com>
2024-03-31 18:27:13 -0400

    Update MessageException.java

--- MessageException.java
+++ MessageException.java
@@ -1,8 +1,13 @@
 /**
- * Project 4 - MessageException
+ * Message Exception
  * 
  * This class is an exception to be thrown when the sender or recipient of a message
  * can't be found.
+ *
+ * @author Lisa Luo, Zixian Liu, Viswanath Nair, Braeden Patterson, Alexia Gil, lab sec 13
+ *
+ * @version March 31, 2024
+ *
  */
 public class MessageException extends Exception{
     public MessageException(String message) {

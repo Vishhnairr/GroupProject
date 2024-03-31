efd53a57d9d3b4f948c6bf7a6558c82b896759c0
chubbchubb53 <165428849+chubbchubb53@users.noreply.github.com>
2024-03-31 16:58:26 -0400

    Update MessageException.java
    Added class description

--- MessageException.java
+++ MessageException.java
@@ -1,3 +1,9 @@
+/**
+ * Project 4 - MessageException
+ * 
+ * This class is an exception to be thrown when the sender or recipient of a message
+ * can't be found.
+ */
 public class MessageException extends Exception{
     public MessageException(String message) {
         super(message);

a60b1cfea89d74dea069f0d52205a7ae441630b1
gil10Coding <115641776+gil10Coding@users.noreply.github.com>
2024-04-28 22:50:20 -0400

    Update ClientList.java

--- ClientList.java
+++ ClientList.java
@@ -1,18 +1,16 @@
 /**
- * ClientList
+ * Client List
  *
- * This class runs the GUI that users will interact with when they use
- * the social platform. It stores the commands and data that the users choose
- * and input to send to the platform's server and displays interfaces
- * based on data received from the server.
+ * This class is an interface of the Clients class,
+ * which allows the server to connect to the client.
  *
  * @author Lisa Luo, Zixian Liu, Viswanath Nair, Braeden Patterson, Alexia Gil, lab sec 13
  *
- * @version April 26, 2024
+ * @version April 28, 2024
  *
  */
 
 public interface ClientList {
-    void disconnect(); // Method to disconnect the client
-}
+    boolean connectServer();
 
+}
